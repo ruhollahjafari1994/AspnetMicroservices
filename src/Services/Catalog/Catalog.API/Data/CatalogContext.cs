1e8c408bfa3236ed29518acc323f246f38ff8a3d
ruhollah_jafari <30493016+ruhollahjafari1994@users.noreply.github.com>
2021-08-16 17:37:38 +0430

    Some Changes!

--- src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -13,9 +13,10 @@ namespace Catalog.API.Data
         public CatalogContext(IConfiguration configuration)
         {
             var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
-            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
             Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
-             CatalogContextSeed.SeedData(Products);
+            CatalogContextSeed.SeedData(Products);
         }
         public IMongoCollection<Product> Products { get; }
     }
