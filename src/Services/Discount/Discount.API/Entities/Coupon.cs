0be25ccc2f9d8352a37ceef74def29206de417e1
ruhollah_jafari <30493016+ruhollahjafari1994@users.noreply.github.com>
2021-08-27 23:53:43 +0430

    Add Discount.API

---
+++ src/Services/Discount/Discount.API/Entities/Coupon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.API.Entities
+{
+    public class Coupon
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public int Amount { get; set; }
+
+    }
+}
