addc6e1c57c3a3a560dd07efbc7522f6e8916da9
ruhollah_jafari <30493016+ruhollahjafari1994@users.noreply.github.com>
2021-09-17 23:25:06 +0430

    Discount Grpc

---
+++ src/Services/Discount/Discount.Grpc/Mapper/DiscountProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Discount.Grpc.Protos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc.Mapper
+{
+    public class DiscountProfile : Profile
+    {
+        public DiscountProfile()
+        {
+            CreateMap<Entities.Coupon, CouponModel>().ReverseMap();
+        }
+    }
+}
---
+++ src/Services/Discount/Discount.Grpc/Program.cs
@@ -0,0 +1,30 @@
+using Discount.Grpc.Extensions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discount.Grpc
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+            host.MigrateDatabase<Program>(); // after build and migrate to database 
+            host.Run(); // we can run application
+        }
+
+        // Additional configuration is required to successfully run gRPC on macOS.
+        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
